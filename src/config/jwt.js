581969ff1e543b40d30644e9b10a9e5e1a725985
Minh <lightstormlxag93@gmail.com>
2025-09-18 00:23:53 +0700

    up

--- src/config/jwt.js
+++ src/config/jwt.js
@@ -6,11 +6,11 @@ export const createToken = (data) => {
     return jwt.sign({ data: data }, "BI_MAT", { expiresIn: "5d" })
 }
 
-// tạo token lop
+// gia hạn token trong lớp
 export const createTokenL = () => {
     return jwt.sign({
         "tenLop": "Bootcamp Sáng 10",
-        "HetHanString": "01/09/2025",
+        "HetHanString": "01/09/2027",
         "HetHanTime": "1725148800000"
     }, "BI_MAT", { expiresIn: "5d" })
 }
